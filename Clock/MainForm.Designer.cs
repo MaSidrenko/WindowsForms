09994148851829f3c80a95809ca1963938854a07
Sidor_md45@top-academy.ru <Sidor_md45@top-academy.ru>
2025-01-16 21:28:45 +0300

    SetPlayerInviseble DONE

--- Clock/MainForm.Designer.cs
+++ Clock/MainForm.Designer.cs
@@ -259,6 +259,7 @@
 			this.axWindowsMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer.OcxState")));
 			this.axWindowsMediaPlayer.Size = new System.Drawing.Size(262, 47);
 			this.axWindowsMediaPlayer.TabIndex = 4;
+			this.axWindowsMediaPlayer.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.SetPlayerInvisible);
 			// 
 			// MainForm
 			// 
